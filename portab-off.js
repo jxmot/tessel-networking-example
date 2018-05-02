3f160fa869b59f03e8257a06842b5bb03007d9ba
jxmot <webdev@stupiddog.net>
2018-05-01 23:30:47 -0500

    simple app that turns the Port LEDs off

---
+++ portab-off.js
@@ -0,0 +1,24 @@
+// jxmot - this file is only meant to demonstrate that
+// it's possible to turn the ports(A & B) off.
+
+'use strict';
+
+// Import the interface to Tessel hardware
+const tessel = require('tessel');
+
+// jxmot - turn off ports A and B, not in use so turn off
+// the LEDs
+tessel.close();
+
+// Turn one of user the LEDs on to start.
+tessel.led[2].on();
+
+// Blink! This will alternate between the 2 user 
+// LEDs (green and blue).
+setInterval(() => {
+  tessel.led[2].toggle();
+  tessel.led[3].toggle();
+}, 500);
+
+console.log("I'm blinking! (Press CTRL + C to stop)");
+

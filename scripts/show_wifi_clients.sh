e52c5b8228f61fbdf570f64567b3d9552a37c655
jxmot <webdev@stupiddog.net>
2018-05-07 10:16:23 -0500

    comments and minor changes

--- scripts/show_wifi_clients.sh
+++ scripts/show_wifi_clients.sh
@@ -15,10 +15,13 @@
 #   ./show_wifi_clients.sh
 #
 #
-#  Create a JSON string for each station
+#  Create an array of JSON strings. One element for each station
 #   ./show_wifi_clients.sh json
 #
 #
+
+# look for the "json" argument, and enable JSON output 
+# if present.
 jsonout=0
 if [ "$1" = "json" ]; then
   jsonout=1
@@ -31,6 +34,7 @@ if [ $jsonout -eq 0 ]; then
   echo    "# hostname (if available), and MAC address."
   echo -e "# IP address\tname\tMAC address"
 else
+  # output format is JSON, start an array
   echo "["
 fi
 
@@ -41,6 +45,7 @@ do
   # for each interface, get mac addresses of connected stations/clients
   maclist=$(iw dev $interface station dump | grep Station | cut -f 2 -s -d" ")
 
+  # count the MACs that were found
   mcount=$(echo "$maclist" | wc -w)
 
   # for each mac address in that list...
@@ -56,18 +61,21 @@ do
     if [ $jsonout -eq 0 ]; then
       echo -e "$ip\t$host\t$mac"
     else
+      # output data as JSON
       tstamp=$(cat /tmp/dhcp.leases | grep $mac | cut -f 1 -s -d" ")
-      lineout="{\"tstamp\":$tstamp,\"ip\":\"$ip\",\"host\":\"$host\",\"mac\":\"$mac\"}"
+      echo -n "{\"tstamp\":$tstamp,\"ip\":\"$ip\",\"host\":\"$host\",\"mac\":\"$mac\"}"
+      # decrement the count, if > 0 then append a comma
       mcount=$((mcount - 1))
       if [ $mcount -gt 0 ]; then
-        echo "$lineout,"
+        echo ","
       else
-        echo "$lineout"
+        echo ""
       fi
     fi
   done
 done
 
+# close the JSON array
 if [ $jsonout -eq 1 ]; then
   echo "]"
 fi

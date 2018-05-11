c2988a1477ab9c27c94e192d5b6fba28d69c2b86
jxmot <webdev@stupiddog.net>
2018-05-11 03:10:55 -0500

    initial commit, responds to GET

---
+++ tessel-ap-http.js
@@ -0,0 +1,44 @@
+//'use strict';
+
+// Node modules - 
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+/* ************************************************************************ */
+// Events
+const EventEmitter = require('events');
+
+module.exports = (function() {
+
+    httpsrv = {};
+
+    httpsrv.init = function(ipaddr) {
+        console.log('starting up http server on '+ipaddr);
+
+        // Create the server, and wait for a connection....
+        var serverhttp = http.createServer();
+
+        serverhttp.on('listening', () => {
+            console.log('server is listening');
+        });
+
+        serverhttp.on('error', (err) => {
+            console.log(err);
+        });
+
+        serverhttp.listen(80, ipaddr);
+
+        serverhttp.on('request',(req, res) => {
+            console.log('method = ' + req.method);
+            if(req.method == 'GET') {
+                //console.log(JSON.parse(req, null, 2));
+                //console.log(req);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write('<h1>Yo</h1>');
+                res.end();
+            }
+        });
+    };
+
+    return httpsrv;
+})();

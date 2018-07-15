b38aa9f35ea02462e228522c350bf4a31f6b5350
jxmot <webdev@stupiddog.net>
2018-07-14 20:42:10 -0500

    very minor change

--- tessel-ap-http.js
+++ tessel-ap-http.js
@@ -85,12 +85,13 @@ function servePath(req, res, wwwpath, wwwcomm) {
         if(!exist) {
             if(pathname.includes('favicon.ico') === true) {
                 con.log('httpsrv.servePath() : favicon.ico request, responding with 204');
-                res.status(204).send('/favicon.ico does not exist');
+                res.status(204).send('/favicon.ico not found');
                 res.end();
             } else {
                 if(pathname.includes('404.css') === true) {
                     serveFile(path.join(wwwcomm, '/assets/css/404.css'), res);
                 } else {
+// check wwwcomm + pathname for exist before 404
                     res.statusCode = 404;
                     con.log(`httpsrv.servePath() : File ${pathname} not found!`);
                     serveFile(path.join(wwwcomm, '404.html'), res);
